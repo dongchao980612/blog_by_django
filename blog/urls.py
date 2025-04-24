a81095987f715de28baac1c7de3707e32ae229c4
dongchao612 <dongchao980612@163.com>
2025-04-24 12:56:16 +0800

    update  uel

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 import blog.views
 
 urlpatterns = [
-    path("hello_world/", blog.views.hello_world),
-    path("content/", blog.views.article_content),
+    # path("hello_world/", blog.views.hello_world),
+    # path("content/", blog.views.article_content),
     # path("index/<int:page>/", blog.views.get_index_page),
     path("index/", blog.views.get_index_page),
     path("detail/<int:article_id>/", blog.views.get_detail_page),
